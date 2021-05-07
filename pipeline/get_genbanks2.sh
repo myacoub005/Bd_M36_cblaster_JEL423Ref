dce7c65c3b101d4e5b3505312e3a49e637bcb903
Mark Yacoub <myaco005@parrot.ib.hpcc.ucr.edu>
2021-05-07 13:11:52 -0700

    added get strains

---
+++ pipeline/get_genbanks2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -p short -N 1 -n 16 --mem 16gb --out getgbks_1job.log
+
+mkdir strains
+#make the sym link
+ln -s /bigdata/stajichlab/shared/projects/Chytrid/Bd_popgen/annotate_scaffold/funannotate 
+cp /funannotate/*/update_results/*.gbk /strains/
