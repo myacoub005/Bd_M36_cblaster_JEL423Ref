eabac1c1763359fbe69db217fb22c4a6dda110b9
Mark Yacoub <myaco005@parrot.ib.hpcc.ucr.edu>
2021-05-07 13:59:49 -0700

    updates

--- step3_make_query_gbks.py
+++ step3_make_query_gbks.py
@@ -5,5 +5,20 @@ module unload miniconda2
 module load miniconda3
 from Bio import SeqIO
 record_dict = SeqIO.index("GCA_000149865.1_BD_JEL423_genomic.gbff", "genbank")
-chrom1 = record_dict['DS022321.1']
-chrom1_subseq = chrom1[START:STOP]
+chrom1 = record_dict['DS022300.1']
+chrom1_subseq = chrom1[1012752:1027530]
+SeqIO.write(chrom1_subseq, "chrom1.1", "gbk")
+
+chrom1_subseq = chrom1[1081890:1097153]
+SeqIO.write(chrom1_subseq, "chrom1.2", "gbk")
+
+chrom2 = record_dict['DS022301.1']
+chrom2_subseq = chrom2[15145:24395]
+SeqIO.write(chrom2_subseq, "chrom2.1", "gbk")
+
+chrom2_subseq = chrom2[1650622:1664965]
+SeqIO.write(chrom2_subseq, "chrom2.2", "gbk")
+
+chrom3 = record_dict['DS022302.1']
+chrom3_subseq = chrom3[692174:706871]
+SeqIO.write(chrom3_subseq, "chrom3.1", "gbk")
