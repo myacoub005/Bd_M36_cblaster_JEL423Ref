45b4b2ca51162bc8baf1231da53ea2806a9a8b0f
Jason Stajich <jasonstajich.phd@gmail.com>
2021-05-11 23:52:12 -0700

    migrate to more compact parallel run for speed

--- pipeline/01_sync_genbanks.sh
+++ pipeline/01_sync_genbanks.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-#SBATCH -p short -N 1 -n 1 --mem 2gb --out getgbks_1job.log
+#SBATCH -p short -N 1 -n 32 --mem 32gb --out logs/getgbks_1job.log
 
-mkdir -p strains
-#make the sym link
+mkdir -p strains DMND
+CPU=$SLURM_CPUS_ON_NODE
+if [ -z $CPU ]; then
+  CPU=1
+fi
+
 INDIR=/bigdata/stajichlab/shared/projects/Chytrid/Bd_popgen/annotate_scaffold/funannotate
 
+#make the sym link
 #add the strain gbks to one location
-rsync -a $INDIR/funannotate/*/update_results/*.gbk strains/
+ln -s $INDIR/*/update_results/*.gbk strains/
+
+module load cblaster
+module load diamond
+module load parallel
+parallel -j $CPU cblaster makedb {} -n DMND/{/.}_ref ::: $(ls strains/*.gbk)
