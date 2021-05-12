38218e1e9c7dd37035b59b507df94ee77e87ab5c
Jason Stajich <jasonstajich.phd@gmail.com>
2021-05-11 23:53:45 -0700

    make output files .txt instead of .csv

--- pipeline/03_cblaster_search.sh
+++ pipeline/03_cblaster_search.sh
@@ -17,6 +17,6 @@ mkdir -p $OUT
 for q in $(ls gbk_queries/*.gbk)
 do
 	qname=$(basename $q .gbk)
-	parallel -j $CPU cblaster search -m local -d DMND/{/.}_ref.dmnd -qf $q -me 1e-100 -o $OUT/{/.}_${qname}_output.csv -ode "," ::: $(ls strains/*.gbk)
-	parallel -j $CPU cblaster search -m local -d DMND/{/.}_ref.dmnd -qf $q -me 1e-100 -o $OUT/{/.}_${qname}_output_binary.csv -bde "," ::: $(ls strains/*.gbk)
+	parallel -j $CPU cblaster search -m local -d DMND/{/.}_ref.dmnd -qf $q -me 1e-100 -o $OUT/{/.}_${qname}_output.txt -ode "," ::: $(ls strains/*.gbk)
+	parallel -j $CPU cblaster search -m local -d DMND/{/.}_ref.dmnd -qf $q -me 1e-100 -o $OUT/{/.}_${qname}_output_binary.txt -bde "," ::: $(ls strains/*.gbk)
 done
